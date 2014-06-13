aababed01ab5f3c0b73895601a0dc1bc2d97080a
Brent <bjgaynor@gmail.com>
2014-06-13 03:41:14 -0700

    exception handling returned

--- Gemfile
+++ Gemfile
@@ -2,14 +2,22 @@ source 'https://rubygems.org'
 
 ruby '2.0.0'
 
+#Rails
   gem 'rails', '4.0.0'
+  gem 'sass-rails', '~> 4.0.0'
+  gem 'uglifier', '>= 1.3.0'
+  gem 'coffee-rails', '~> 4.0.0'
+  gem 'jquery-rails'
+  gem 'turbolinks'
+  gem 'jbuilder', '~> 1.2'
+
+#Me
   gem 'pg'
   gem 'thin'
   gem 'simple_form'
   gem 'rubillow'
 
 group :development, :test do
-  # gem 'sqlite3'
   gem 'rspec-rails'
   gem 'faker'
   gem 'capybara'
@@ -21,12 +29,7 @@ group :development, :test do
   gem 'selenium-webdriver'
 end
 
-  gem 'sass-rails', '~> 4.0.0'
-  gem 'uglifier', '>= 1.3.0'
-  gem 'coffee-rails', '~> 4.0.0'
-  gem 'jquery-rails'
-  gem 'turbolinks'
-  gem 'jbuilder', '~> 1.2'
+
 
 group :doc do
   gem 'sdoc', require: false
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -5,18 +5,18 @@ class ListingsController < ApplicationController
   end
 
   def create
-    # begin
+    begin
     @listing = Listing.create(listing_params)
     @search_data = Rubillow::PropertyDetails.deep_search_results({ :address => @listing.address, :citystatezip => @listing.city_state_zip, :rentzestimate => true })
     @listing.update_attributes(zpid: @search_data.zpid, rent_zestimate: @search_data.rent_zestimate, homedetail_links: @search_data.links)
     @zestimate_data = Rubillow::HomeValuation.zestimate({ :zpid => @search_data.zpid })
     @listing.update_attributes(zestimate: @zestimate_data.price) #, forecast_percentage: @zestimate_data.percentile)
-    # rescue => e
-    #   render :error
-    #   return
-    # else
+    rescue => e
+      render :error
+      return
+    else
     render :create
-    # end
+    end
   end
 
   private
