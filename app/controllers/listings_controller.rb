c692dba5db986d84547b18289a82669a15dd252b
Brent <bjgaynor@gmail.com>
2014-06-19 02:14:22 -0700

    mechanize can read all elements off the page, unfortunately, elements are not present

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -10,6 +10,13 @@ class ListingsController < ApplicationController
     @search_data = Rubillow::PropertyDetails.deep_search_results({ :address => @listing.address, :citystatezip => @listing.city_state_zip, :rentzestimate => true })
     @zestimate_data = Rubillow::HomeValuation.zestimate({ :zpid => @search_data.zpid })
     @listing.update_attributes(zpid: @search_data.zpid, zestimate: @zestimate_data.price, rent_zestimate: @search_data.rent_zestimate, homedetail_links: @search_data.links)
+
+    # mechanize = Mechanize.new
+    # page = mechanize.get("#{@listing.homedetail_links.first[1]}")
+    # puts page.search('.zest-container').inner_text
+
+    # User must log in to see the contents of this container including the Zestimate Forecast
+
     rescue => e
       render :error
       return
