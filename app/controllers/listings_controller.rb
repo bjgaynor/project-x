51d3637c54dbce006f27e1d4b812666ac46dd849
Brent <bjgaynor@gmail.com>
2014-06-20 05:22:35 -0700

    can scrape the forecast percentage from the zillow website using mechanize

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -5,24 +5,38 @@ class ListingsController < ApplicationController
   end
 
   def create
-    begin
+    # begin
     @listing = Listing.create(listing_params)
     @search_data = Rubillow::PropertyDetails.deep_search_results({ :address => @listing.address, :citystatezip => @listing.city_state_zip, :rentzestimate => true })
     @zestimate_data = Rubillow::HomeValuation.zestimate({ :zpid => @search_data.zpid })
     @listing.update_attributes(zpid: @search_data.zpid, zestimate: @zestimate_data.price, rent_zestimate: @search_data.rent_zestimate, homedetail_links: @search_data.links)
-
-    # mechanize = Mechanize.new
-    # page = mechanize.get("#{@listing.homedetail_links.first[1]}")
-    # puts page.search('.zest-container').inner_text
-
-    # User must log in to see the contents of this container including the Zestimate Forecast
-
-    rescue => e
-      render :error
-      return
-    else
-    render :create
+    mechanize = Mechanize.new do |agent|
+      agent.follow_meta_refresh = true
+      agent.redirect_ok = true
     end
+    page = mechanize.get("#{@listing.homedetail_links.first[1]}") do |home_page|
+    login_page = mechanize.click(home_page.link_with(:text => 'Sign In'))
+    my_page = login_page.form_with(:id => 'loginForm') do |form|
+      email_field = form.field_with(:id => 'email')
+      email_field.value = 'Bjgaynor@gmail.com'
+      password_field = form.field_with(:id => 'password')
+      password_field.value = 'b050295g'
+      next_page = mechanize.submit(form)
+      # puts next_page.search('.zest-value').inner_text
+      puts next_page.search('.zest-value').inner_text
+      @listing.update_attributes(forecast_percentage: next_page.search('.zest-forecast-change-percent').inner_text)
+      puts @listing.inspect
+    end
+  end
+
+  # end
+
+    # rescue => e
+    #   render :error
+    #   return
+    # else
+    render :create
+    # end
   end
 
   private
