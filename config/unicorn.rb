60126205413b668ffa11987e43652593dcc4fbcd
Brent <bjgaynor@gmail.com>
2014-06-25 06:45:00 -0700

    add sidekiq to unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,3 +4,13 @@ timeout 120
 # Enable streaming (for excel downloads)
 port = (ENV["PORT"] || 3000).to_i
 listen port, :tcp_nopush => false
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://redis.example.com:7372/12' }
+
+  database_url = ENV['DATABASE_URL']
+  if database_url
+    ENV['DATABASE_URL'] = "#{database_url}?pool=20"
+    ActiveRecord::Base.establish_connection
+  end
+end
