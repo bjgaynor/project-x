bc6e2ff0f8a77f5798f323498372845bfc959e51
Brent <bjgaynor@gmail.com>
2014-06-25 09:28:12 -0700

    further edit

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,10 +4,3 @@ timeout 120
 # Enable streaming (for excel downloads)
 port = (ENV["PORT"] || 3000).to_i
 listen port, :tcp_nopush => false
-
-
-  database_url = ENV['DATABASE_URL']
-  if database_url
-    ENV['DATABASE_URL'] = "#{database_url}?pool=20"
-    ActiveRecord::Base.establish_connection
-  end
