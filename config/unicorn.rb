e4c68a3faaccc2edb4269797911c370e600da34e
Brent <bjgaynor@gmail.com>
2014-06-25 05:58:33 -0700

    add unicorn.rb code again

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
-# worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-# timeout 120
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 120
 
-# # Enable streaming (for excel downloads)
-# port = (ENV["PORT"] || 3000).to_i
-# listen port, :tcp_nopush => false
+# Enable streaming (for excel downloads)
+port = (ENV["PORT"] || 3000).to_i
+listen port, :tcp_nopush => false
